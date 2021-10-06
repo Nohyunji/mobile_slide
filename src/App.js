cbbcfad6a7b06e4d9d3053382dbcc9493fb0f879
Nohyunji <t4933@naver.com>
2021-10-06 18:04:02 +0900

    프로젝트 소스 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React ,{ useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+  const [slide, SetSlide] = useState(false);
+
+  const media = [
+    {
+      id: 1,
+      url:"/img/couple.mp4"
+    },{
+      id: 2,
+      url:`/img/diving.mp4`
+    },
+    // {
+    //   id: 3,
+    //   url:`/img/lanterns-phone.mp4`
+    // }
+  ]
+
+  useEffect(()=>{
+    if(slide === false) {
+      SetSlide(true);
+
+      setTimeout(() => {
+
+        media.pop(media[0]);
+
+        console.log("media::", media);
+        
+        // SetSlide(false);
+      }, 5000);
+    }
+
+  },[slide])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          123
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app_wrap">
+        <div className="content">
+          <img src ="/img/mobile.png"/>
+
+          <div className={ slide ? `media_list slide_time` : `media_list`}>
+            {media.map((media, index) => 
+              <video autoPlay loop muted key={index}>
+                <source src={media.url} type="video/mp4" />
+              </video> 
+            )}           
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
